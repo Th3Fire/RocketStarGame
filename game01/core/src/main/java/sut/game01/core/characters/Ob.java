3df3bb517b2b56babe586b3cbf66fabf7d779443
wuttinunt <wuttinunt.th@gmail.com>
2016-06-01 12:26:52 +0700

    for send

--- game01/core/src/main/java/sut/game01/core/characters/Ob.java
+++ game01/core/src/main/java/sut/game01/core/characters/Ob.java
@@ -1,10 +1,8 @@
 package sut.game01.core.characters;
 
-
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.*;
-import org.jbox2d.dynamics.contacts.Contact;
 import playn.core.Layer;
 import playn.core.PlayN;
 import playn.core.util.Callback;
@@ -15,38 +13,44 @@ import sut.game01.core.GameplayScreen3;
 import sut.game01.core.sprite.Sprite;
 import sut.game01.core.sprite.SpriteLoader;
 
-public class line {
+public class Ob {
     private Sprite sprite;
     private int spriteIndex = 0;
     private boolean hasLoaded = false;
-    private int checkDestroy = 0;
+    private boolean checkDestroy = false;
     private World world;
 
+
     public enum State {
-        IDLE, Go, RIGHT
+        RED, GREEN, BOOM
     };
 
-    public State state = State.IDLE;
+    public State state = State.RED;
 
     private int e = 0;
-    private int offset = 0; //chang Here
+    private int offset = 8; //chang Here
 
     //public float dx = 300f,dy = 400f;
 
     private float x; //<<
     private float y; //<<
     private Body body; //<<
-    private int index = 1;
+    private int _index = 1;
 
+    public Ob(final World world, final float _x, final float _y, int index,int r){  //<<
 
-    public line(final World world, final float _x, final float _y,int index){  //<<
+        if(r == 1){
+            //state = State.RED;
+        }else{
+            //state = State.GREEN;
+        }
 
         this.x = _x; //<<
         this.y = _y; //<<
         this.world = world;
-        this.index = index;
+        this._index = index;
 
-        sprite = SpriteLoader.getSprite("images/line.json");
+        sprite = SpriteLoader.getSprite("images/ob.json");
         sprite.addCallback(new Callback<Sprite>() {
             @Override
             public void onSuccess(Sprite result) {
@@ -55,7 +59,6 @@ public class line {
                 sprite.layer().setOrigin(sprite.width() / 2f, sprite.height() / 2f);
                 sprite.layer().setTranslation(x,y + 3f);  //<<
                 body = initPhysicsBody(world, GameplayScreen.M_PER_PIXEL * x, GameplayScreen.M_PER_PIXEL * y);  //<<
-
                 hasLoaded = true;
             }
 
@@ -76,17 +79,29 @@ public class line {
         if(hasLoaded == false) return;
 
 
-
         e += delta;
         if(e > 150) {
 
             switch (state){
-                case IDLE: offset = 0; break;
-                case Go: offset = 4; break;
-                case RIGHT: offset = 8; break;
+                case RED: offset = 0; break;
+                case GREEN: offset = 4; break;
+                case BOOM: offset = 8; break;
 
             }
+            if(checkDestroy){
+                //world.destroyBody(body);
+                System.out.println("world destroy work.");
+            }
 
+            if(state == State.BOOM){
+
+                if(spriteIndex == 11){
+                    //sprite.layer().destroy();
+                    //world.destroyBody(body);
+                    sprite.layer().setVisible(false);
+
+                }
+            }
 
             spriteIndex = offset + ((spriteIndex + 1)%4);
             sprite.setSprite(spriteIndex);
@@ -99,23 +114,23 @@ public class line {
 
     private Body initPhysicsBody(World world, float x, float y){  //<< Start
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyType.STATIC;
+        bodyDef.type = BodyType.DYNAMIC;
         bodyDef.position = new Vec2(200/26.666667f, 200/26.666667f);
         Body body = world.createBody(bodyDef);
         PolygonShape shape = new PolygonShape();
 
-        if(index == 1) {
-            GameplayScreen.bodies.put(body, "line" );
-            GameplayScreen.bodiesB.put(body, "line");
-            shape.setAsBox(640,1);
-        }else if(index == 2){
-            GameplayScreen2.bodies.put(body, "line" );
-            GameplayScreen2.bodiesB.put(body, "line");
-            shape.setAsBox(640,1);
-        }else if(index == 3){
-            GameplayScreen3.bodies.put(body, "line");
-            GameplayScreen3.bodiesB.put(body, "line");
-            shape.setAsBox(640,1);
+        if(_index == 1) {
+            GameplayScreen.bodies.put(body, "OB" );
+            GameplayScreen.bodiesB.put(body, "OB");
+            shape.setAsBox(30 * GameplayScreen.M_PER_PIXEL / 2, sprite.layer().height() * GameplayScreen.M_PER_PIXEL / 2);
+        }else if(_index == 2){
+            GameplayScreen2.bodies.put(body, "OB" );
+            GameplayScreen2.bodiesB.put(body, "OB");
+            shape.setAsBox(30 * GameplayScreen2.M_PER_PIXEL / 2, sprite.layer().height() * GameplayScreen2.M_PER_PIXEL / 2);
+        }else if(_index == 3){
+            GameplayScreen3.bodies.put(body, "OB");
+            GameplayScreen3.bodiesB.put(body, "OB");
+            shape.setAsBox(30 * GameplayScreen3.M_PER_PIXEL / 2, sprite.layer().height() * GameplayScreen3.M_PER_PIXEL / 2);
         }
 
 
@@ -124,8 +139,12 @@ public class line {
         fixtureDef.density = 1.0f;
         fixtureDef.friction = 0.1f;
         fixtureDef.restitution = 0f;
+        fixtureDef.filter.groupIndex = -1;
+        //body.setBullet(true);
         body.createFixture(fixtureDef);
-        body.setFixedRotation(true);
+
+        //body.createFixture(fixtureDef);
+
         body.setLinearDamping(0.2f);
         body.setTransform(new Vec2(x, y), 0f);
 
@@ -136,9 +155,14 @@ public class line {
         return body;
     }
 
-    public void contact (Contact contact, String s){
+    /*public void contact(Contact contact,String s,Ufo2 ufo2){
+
+        checkDestroy = true;
+        //GameplayScreen.deleteUfo2.add(ufo2);
+        System.out.println("metheod contact work.");
 
     }
+    */
 
 
     public void paint(Clock clock){
